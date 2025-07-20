bab05a0eaa3aec6442a0b225ac6986e6a9970f12
Yazhinikanchi <yazhinip1215@gmail.com>
2025-07-20 20:39:22 +0530

    adding a file

---
+++ problem1.py
@@ -0,0 +1,9 @@
+def sumOfMultiplesOf3And5(limit):
+    sum = 0
+    for x in list([n for n in range(1,limit) if n % 3 == 0 or n % 5 == 0]):  #numbers:
+        # print(x)
+        sum += x
+    return(sum)
+
+if __name__ == '__main__':
+    print(sumOfMultiplesOf3And5(10))
