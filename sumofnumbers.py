858a96434ffef323b78858c54347332d3fa22e2a
yazhini <yazhinip1215@gmail.com>
2025-08-16 21:05:09 +0530

    Add files via upload

---
+++ sumofnumbers.py
@@ -0,0 +1,33 @@
+"""answer=0
+for i in range(11):
+    answer=answer+i
+    print(answer) this gives step by step answers lyk 0,1,3,....and so on
+"""
+
+
+'''
+sum=0
+for i in range(100):
+    sum=sum+i
+print(sum)
+#this gives final answer for thizz 4950...
+
+#Multiplication table
+y=int(input("Which table would you like to display now?"))
+for i in range(1,11):
+    print(y,"X",i,"=",y*i)
+
+#using while loop
+print("hello everyone, we r starting!")
+c=1
+n=1
+while(c==1):
+    print("token number",n,"can come in")
+    c=input("order(0/1)")
+    c=int(c)
+    n=n+1
+print("thank you,this is the end of our day")
+'''
+
+
+
